e7f627f473c439c46d7e5f12ee61c4bf93872402
Nattaphong56 <67319010007@tnk.ac.th>
2024-10-30 18:05:57 +0700

    Resume.py

---
+++ Resume.py
@@ -0,0 +1,15 @@
+name = (input ("โปรดกรอกชือ : \n"))
+age = (input("โปรดกรอกอายุ :\n"))
+number = (input("โปรดกรอกรหัสประจำตัว : \n"))
+level = (input("โปรดกรอกระดับชั้น :\n"))
+nickname =(input("โปรดกรอกชื่อเล่น :\n"))
+height = (input("โปรดกรอกส่วนสูง :\n"))
+weight =(input("โปรดกรอกน้ำหนัก :\n"))
+print("ชื่อ: "+ name + "\tอายุ : " + age + "ปี")
+print("รหัสประจำตัว :  "+ number + "\tระดับชั้น : " + level )
+print("ส่วนสูง :  "+ height + "เซนติเมตร" "\tน้ำหนัก : " + weight +"กิโลกรัม")
+
+n = height + weight
+print("ส่วนสูง + น้ำหนัก =" ,(n))
+
+
